655f0340f54abf68557214a98af123869598aa77
Colo Champre <colochampre3@gmail.com>
2025-11-09 15:51:47 -0300

    maqueta inicial

--- apuntes/apuntes/urls.py
+++ apuntes/apuntes/urls.py
@@ -21,8 +21,8 @@ from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    # path('usuarios/', include('gestion_usuarios.urls'), namespace='gestion_usuarios'),
-    # path('apuntes/', include('gestion_apuntes.urls'), namespace='gestion_apuntes'),
-    # path('carreras/', include('gestion_carreras.urls'), namespace='gestion_carreras'),
-    # path('materias/', include('gestion_materias.urls'), namespace='gestion_materias'),
+    # path('usuarios/', include(('gestion_usuarios.urls', gestion_usuarios), namespace='gestion_usuarios')),
+    path('apuntes/', include(('gestion_apuntes.urls', 'gestion_apuntes'), namespace='gestion_apuntes')),
+    # path('carreras/', include(('gestion_carreras.urls', 'gestion_carreras'), namespace='gestion_carreras')),
+    # path('materias/', include(('gestion_materias.urls', 'gestion_materias'), namespace='gestion_materias')),
 ]
