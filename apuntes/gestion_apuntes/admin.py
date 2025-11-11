4381e94a8403bb28a36848ac0ea131c2975e42f8
lautaro <cardenaslautaro8@gmail.com>
2025-11-11 19:16:04 -0300

    models y admins

--- apuntes/gestion_apuntes/admin.py
+++ apuntes/gestion_apuntes/admin.py
@@ -3,20 +3,5 @@ from .models import Apunte
 
 @admin.register(Apunte)
 class ApunteAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'materia', 'usuario', 'fecha_publicacion')  # columnas visibles en la lista
-    list_filter = ('materia', 'fecha_publicacion')  # filtros laterales
-    search_fields = ('titulo', 'descripcion', 'usuario__user__username')  # barra de búsqueda
-    ordering = ('titulo',)
-    readonly_fields = ('fecha_publicacion',)  # evita modificar la fecha desde el admin
-
-    fieldsets = (
-        ('Información general', {
-            'fields': ('titulo', 'descripcion', 'archivo')
-        }),
-        ('Relaciones', {
-            'fields': ('materia', 'usuario')
-        }),
-        ('Metadatos', {
-            'fields': ('fecha_publicacion',),
-        }),
-    )
+    list_display = ("usuario", "titulo", "materia", "fecha_publicacion")
+    ordering = ("fecha_publicacion", "materia")
