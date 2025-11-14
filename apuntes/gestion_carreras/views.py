755cab09317ce5fb0a60cccbb006002b782dfa31
Colo Champre <colochampre3@gmail.com>
2025-11-14 14:27:27 -0300

    Dropdown de login + responsive

--- apuntes/gestion_carreras/views.py
+++ apuntes/gestion_carreras/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Carrera
+
 # Create your views here.
 def lista_carreras(request):
-    carreras = Carrera.objects.all() # trae todas las carreras de la BD
-    return render(request, 'gestion_carreras/lista_carreras.html', {
-        'carreras': carreras}) # envio la lista de carreras al template
+    carreras = Carrera.objects.all().order_by('nombre') # trae las carreras de la BD ordenadas por nombre
+    return render(request, 'gestion_carreras/lista_carreras.html', {'carreras': carreras})  # envio la lista de carreras al template
