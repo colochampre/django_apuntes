82f36cc4ce9c47edeb5cc084faaa80566ec93e67
JuanPe <juanpedroboda@gmail.com>
2025-11-22 00:17:20 -0300

    Correccion error, no se veian apuntes subisdos, arreglado

--- apuntes/apuntes/urls.py
+++ apuntes/apuntes/urls.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('usuarios/', include(('gestion_usuarios.urls', 'gestion_usuarios'), namespace='gestion_usuarios')),
-    path('apuntes/', include(('gestion_apuntes.urls', 'gestion_apuntes'), namespace='gestion_apuntes')),
-    path('carreras/', include(('gestion_carreras.urls', 'gestion_carreras'), namespace='gestion_carreras')),
-    path('materias/', include(('gestion_materias.urls', 'gestion_materias'), namespace='gestion_materias')),
-    
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path(
+        "usuarios/",
+        include(
+            ("gestion_usuarios.urls", "gestion_usuarios"), namespace="gestion_usuarios"
+        ),
+    ),
+    path(
+        "carreras/",
+        include(
+            ("gestion_carreras.urls", "gestion_carreras"), namespace="gestion_carreras"
+        ),
+    ),
+    path(
+        "materias/",
+        include(
+            ("gestion_materias.urls", "gestion_materias"), namespace="gestion_materias"
+        ),
+    ),
+    path(
+        "apuntes/",
+        include(
+            ("gestion_apuntes.urls", "gestion_apuntes"), namespace="gestion_apuntes"
+        ),
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apuntes/gestion_materias/views.py
+++ apuntes/gestion_materias/views.py
@@ -7,11 +7,23 @@ from gestion_apuntes.models import Apunte
 def listar_materias_por_carrera(request, carrera_id):
     carrera = get_object_or_404(Carrera, id=carrera_id) 
     materias = carrera.materias.all().order_by("nombre")
-    context = {"carrera": carrera, "materias": materias}
-    return render(request, "gestion_materias/lista_materias.html", context)
+    
+    materia_seleccionada = None
+    apuntes = []
 
-def apuntes_por_materia(request, materia_id):
-    materia = get_object_or_404(Materia, id=materia_id)
-    apuntes = Apunte.objects.filter(materia=materia).order_by("-fecha_publicacion")
-    context = {"materia": materia, "apuntes": apuntes}
-    return render(request, "gestion_materias/apuntes_por_materia.html", context)
+    materia_id = request.GET.get('materia_id')
+    if materia_id:
+        materia_seleccionada = get_object_or_404(Materia, id=materia_id)
+    elif materias.exists():
+        materia_seleccionada = materias.first()
+
+    if materia_seleccionada:
+        apuntes = Apunte.objects.filter(materia=materia_seleccionada).order_by("-fecha_publicacion")
+
+    context = {
+        "carrera": carrera, 
+        "materias": materias,
+        "materia_seleccionada": materia_seleccionada,
+        "apuntes": apuntes
+    }
+    return render(request, "gestion_materias/lista_materias.html", context)
