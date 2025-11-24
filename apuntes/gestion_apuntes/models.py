30586334a982d29a8a2d82b9979038cc18a61bbd
lautaro <cardenaslautaro8@gmail.com>
2025-11-24 14:41:07 -0300

    puntuacion-logo

--- apuntes/gestion_apuntes/models.py
+++ apuntes/gestion_apuntes/models.py
@@ -4,6 +4,14 @@ from gestion_usuarios.models import Usuario
 from gestion_materias.models import Materia
 from django.core.exceptions import ValidationError
 
+PUNTUACION = {
+    ("1", "☆"),
+    ("2", "☆☆"),
+    ("3", "☆☆☆"),
+    ("4", "☆☆☆☆"),
+    ("5", "☆☆☆☆☆"),
+}
+
 # Create your models here.
 class Apunte(models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
@@ -12,7 +20,8 @@ class Apunte(models.Model):
     fecha_publicacion = models.DateField(auto_now_add=True)
     materia = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True, blank=True)
     usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True)
-
+    puntuacion = models.CharField(max_length=3, choices= PUNTUACION, null= True, blank=True)
+    
     class Meta:
         ordering = ['titulo']
     
@@ -22,5 +31,5 @@ class Apunte(models.Model):
     
     def __str__(self):
         return (f'{self.titulo} - {self.fecha_publicacion} - '
-                f'{self.usuario.user.username} - {self.fecha_publicacion}')
+                f'{self.usuario.user.username} - {self.fecha_publicacion} - {self.get_puntuacion_display()}')
 
