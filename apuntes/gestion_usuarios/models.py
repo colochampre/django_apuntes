f490c905e0136e7c2fdc60da4fc039952bd23bb9
Colo Champre <colochampre3@gmail.com>
2025-12-11 17:31:18 -0300

    Puntuacion de usuarios

--- apuntes/gestion_usuarios/models.py
+++ apuntes/gestion_usuarios/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from gestion_carreras.models import Carrera
 from django.contrib.auth.models import User
+from django.db.models import Avg, Count
 
 # Create your models here.
 class Usuario(models.Model):
@@ -12,3 +13,41 @@ class Usuario(models.Model):
     
     def __str__(self):
         return f'{self.user.username}'
+
+    def puntuacion_promedio(self):
+        """
+        Calcula el promedio de todas las puntuaciones recibidas en los apuntes del usuario.
+        Retorna un valor entre 1-5 o None si no tiene puntuaciones.
+        """
+        from gestion_apuntes.models import Puntuacion
+        
+        # Obtener todas las puntuaciones de los apuntes del usuario
+        puntuaciones = Puntuacion.objects.filter(apunte__usuario=self)
+        promedio = puntuaciones.aggregate(Avg('valor'))['valor__avg']
+        
+        return round(promedio, 1) if promedio else None
+    
+    def total_puntuaciones_recibidas(self):
+        """
+        Cuenta el total de puntuaciones que han recibido todos los apuntes del usuario.
+        """
+        from gestion_apuntes.models import Puntuacion
+        
+        return Puntuacion.objects.filter(apunte__usuario=self).count()
+    
+    def nivel_reputacion(self):
+        """
+        Retorna un nivel de reputación basado en el promedio de puntuaciones.
+        """
+        promedio = self.puntuacion_promedio()
+        
+        if promedio is None:
+            return "Nuevo"
+        elif promedio >= 4.5:
+            return "Experto"
+        elif promedio >= 4.0:
+            return "Avanzado"
+        elif promedio >= 3.0:
+            return "Intermedio"
+        else:
+            return "Principiante"
